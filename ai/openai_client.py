9fd482712e705a2d88099effa3b8901c2fb509b3
PKT786 <115774041+PKT786@users.noreply.github.com>
2026-06-14 18:39:28 +0530

    Update openai_client.py

--- ai/openai_client.py
+++ ai/openai_client.py
@@ -2,15 +2,22 @@ from openai import OpenAI
 import streamlit as st
 
 
-
 def get_openai_client():
+
+    api_key = st.secrets.get(
+        "OPENAI_API_KEY"
+    )
+
+
+    if not api_key:
+
+        raise Exception(
+            "OPENAI_API_KEY not found in Streamlit secrets"
+        )
 
 
     client = OpenAI(
-
-        api_key=
-        st.secrets["OPENAI_API_KEY"]
-
+        api_key=api_key
     )
 
 
