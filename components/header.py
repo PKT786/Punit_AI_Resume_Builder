76f66dcfe2445cd720aa81f4ef8fd9150f0058cb
PKT786 <115774041+PKT786@users.noreply.github.com>
2026-06-14 18:29:47 +0530

    Update header.py

--- components/header.py
+++ components/header.py
@@ -6,17 +6,51 @@ def hero():
     st.markdown(
     """
 
-# 📄 AI Resume Builder
+<div style="
+padding:40px;
+border-radius:20px;
+background:
+linear-gradient(
+135deg,
+#eef2ff,
+#ecfeff
+);
+">
 
-## Build Your Perfect ATS Optimized Resume
+
+<h1 style="
+font-size:55px;
+color:#111827;
+">
+
+📄 AI Resume Builder
+
+</h1>
 
 
-Create professional resumes using AI.
+<h2>
 
-✨ AI Optimization  
-🛡 ATS Score  
-📄 PDF + Word Export
+Build ATS-Friendly Resume with AI
+
+</h2>
 
 
-    """
-    )
+<p style="
+font-size:20px;
+color:#4b5563;
+">
+
+Generate professional resumes,
+improve keywords,
+match jobs,
+and increase interview chances.
+
+</p>
+
+
+</div>
+
+
+""",
+unsafe_allow_html=True
+)
